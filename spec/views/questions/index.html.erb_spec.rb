4ac9395ccc7b8080d0145198050db5793a7ec977
Randall Reed, Jr <randallreedjr@gmail.com>
2017-06-17 13:31:12 -0400

    Display categories on question list

--- spec/views/questions/index.html.erb_spec.rb
+++ spec/views/questions/index.html.erb_spec.rb
@@ -5,22 +5,30 @@ RSpec.describe "questions/index", type: :view do
     assign(:questions, [
       FactoryGirl.create(
         :question,
-        :title => "Title",
-        :description => "MyText"
+        :with_category,
+        title: "Title",
+        description: "MyText"
       ),
       FactoryGirl.create(
         :question,
-        :title => "Title",
-        :description => "MyOtherText"
+        :with_category,
+        title: "Title",
+        description: "MyOtherText"
       )
     ])
   end
 
   it "renders a list of questions" do
     render
-    assert_select "h3", :text => "Title".to_s, :count => 2
-    assert_select "p", :text => "MyText".to_s, :count => 1
-    assert_select "p", :text => "MyOtherText".to_s, :count => 1
+    assert_select "h3", text: "Title".to_s, count: 2
+    assert_select "p", text: "MyText".to_s, count: 1
+    assert_select "p", text: "MyOtherText".to_s, count: 1
+  end
+
+  it "displays categories for each question" do
+    render
+    assert_select "span", text: Category.first.title, count: 1
+    assert_select "span", text: Category.last.title, count: 1
   end
 
   it "hides edit and delete button" do
