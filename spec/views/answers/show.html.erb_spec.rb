6d052972867a8dc76eef90f4409f0967bb7236e0
Randall Reed, Jr <randallreedjr@gmail.com>
2017-06-16 22:05:58 -0400

    Hide edit and delete buttons if user does not have access

--- spec/views/answers/show.html.erb_spec.rb
+++ spec/views/answers/show.html.erb_spec.rb
@@ -11,4 +11,11 @@ RSpec.describe "answers/show", type: :view do
     expect(rendered).to match(/#{CGI.escapeHTML(@answer.content)}/)
     expect(rendered).to match(/#{CGI.escapeHTML(@question.title)}/)
   end
+
+  it "hides edit and delete button" do
+    # answer not associated with any user
+    render
+    expect(rendered).to_not match(/Edit/)
+    expect(rendered).to_not match(/Delete/)
+  end
 end
